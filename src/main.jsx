89f33085a9ee0fd45f4398dc88885f496acff6c4
Francisco Payés <payesfrancisco@gmail.com>
2024-11-29 18:54:13 -0400

    feat: student form

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,11 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router";
 import './index.css'
 import App from './App.jsx'
+import RegistrationForm from './pages/RegistrationForm.jsx'
 import '@mantine/core/styles.css';
+import '@mantine/dates/styles.css';
+import 'dayjs/locale/es-do.js';
+import { DatesProvider } from '@mantine/dates';
 
 import { MantineProvider, createTheme } from '@mantine/core';
 
@@ -11,9 +15,12 @@ const theme = createTheme({});
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <MantineProvider theme={theme}>
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
+      <DatesProvider settings={{ locale: 'es-do' }}>
+        <Routes>
+          <Route index element={<App />} />
+          <Route path="formulario" element={<RegistrationForm />} />
+        </Routes>
+      </DatesProvider>
     </MantineProvider>
   </BrowserRouter>
 )
---
+++ src/pages/RegistrationForm.jsx
@@ -0,0 +1,124 @@
+import { useState, useEffect } from "react";
+import {
+  Title,
+  Flex,
+  Card,
+  Image,
+  Text,
+  Badge,
+  Button,
+  Group,
+  TextInput,
+  Alert
+} from "@mantine/core";
+import { IconInfoCircle, IconReload } from '@tabler/icons-react';
+import { DatePickerInput } from '@mantine/dates';
+
+export default function RegistrationForm() {
+  const [form, setForm] = useState({
+    name: '',
+    lastName: '',
+    birthDate: '',
+    telephone: '',
+  });
+
+  const [state, setState] = useState('error');
+
+  const isFormValid = form.name && form.lastName && form.birthDate;
+
+  const handleSubmit = () => {
+    try {
+      // TODO: Send form data to server
+      throw new Error("Error al enviar datos al servidor");
+
+      setState('success');
+    } catch {
+      setState('error');
+      return;
+    } finally {
+      setForm({
+        name: '',
+        lastName: '',
+        birthDate: '',
+        telephone: '',
+      });
+    }
+  }
+
+  return (
+    <Flex direction="column" align="center" justify="center" mt={{ md: "xl" }}>
+      <Flex maw={1080} w="100%" justify="center" align="center" direction="column">
+        <Card shadow="sm" padding="lg" radius="md" withBorder w={{ base: "100%", md: "80%" }}>
+          <Card.Section component="a" href="https://mantine.dev/">
+            <Image
+              src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png"
+              height={160}
+              alt="Norway"
+            />
+          </Card.Section>
+
+          <Group justify="space-between" my="md">
+            <Text fw={500}>Formulario registro de estudiante</Text>
+          </Group>
+          {state === 'error' &&
+            <Flex align="center" my="md" gap="md" direction="column">
+              <Alert variant="filled" color="red" radius="md" title="Error al agregar usuario. Por favor intente de nuevo." icon={<IconInfoCircle />} />
+              <Button variant="outline" w="fit-content" color="blue" onClick={() => setState('form')} leftSection={<IconReload size={14} />}>Intentar de nuevo</Button>
+            </Flex>
+          }
+          {(state === 'form') && <>
+            <Flex justify="space-between" my="sm" flex={1}>
+              <Flex flex={1} gap="md" direction={{ base: 'column', md: 'row' }}>
+                <TextInput
+                  flex={1}
+                  label="Nombre"
+                  value={form.name}
+                  required
+                  onChange={(e) => setForm({ ...form, name: e.target.value })}
+                />
+                <TextInput
+                  flex={1}
+                  label="Apellido"
+                  value={form.lastName}
+                  required
+                  onChange={(e) => setForm({ ...form, lastName: e.target.value })}
+                />
+              </Flex>
+            </Flex>
+            <Flex justify="space-between" my="sm" flex={1}>
+              <Flex flex={1} gap="md" direction={{ base: 'column', md: 'row' }}>
+                <DatePickerInput
+                  flex={1}
+                  label="Fecha de nacimiento"
+                  required
+                  value={form.birthDate ? new Date(form.birthDate) : null}
+                  maxDate={new Date()}
+                  onChange={(value) => setForm({ ...form, birthDate: value.getTime() })}
+                />
+                <TextInput
+                  flex={1}
+                  label="Whatsapp"
+                  value={form.telephone}
+                  onChange={(e) =>
+                    setForm({
+                      ...form,
+                      telephone: e.target.value.replace(/[^0-9\-]/g, "")
+                    })
+                  }
+                />
+              </Flex>
+            </Flex>
+            {isFormValid && <Flex justify="center" my="sm" flex={1} align="center">
+              <Button color="green" onClick={handleSubmit}>Guardar</Button>
+            </Flex>}
+          </>}
+          {state === 'success' &&
+            <Flex justify="center" my="md">
+              <Alert variant="filled" color="green" radius="md" title="Estudiante agregado de manera exitosa" icon={<IconInfoCircle />} />
+            </Flex>
+          }
+        </Card>
+      </Flex>
+    </Flex>
+  );
+}
