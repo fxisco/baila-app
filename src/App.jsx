e706d25c2f894101dc64195bfc28b679f620f7b2
Francisco Payés <payesfrancisco@gmail.com>
2024-11-29 19:07:39 -0400

    feat: add menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-import { AppShell, Burger, Group, Title } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
+import { AppShell, Burger, Group, Title, NavLink } from "@mantine/core";
+import { useLocation } from "react-router";
+import { IconUser, IconUsersGroup, IconReceipt2, IconUserStar } from '@tabler/icons-react';
 
 function App() {
   const [opened, { toggle }] = useDisclosure();
+  let { pathname } = useLocation()
 
   return (
     <>
@@ -27,7 +30,30 @@ function App() {
           </Group>
         </AppShell.Header>
         <AppShell.Navbar p="md">
-
+          <NavLink
+            label="Estudiantes"
+            active={pathname === "/"}
+            leftSection={<IconUser />}
+            fw="bolder"
+          />
+          <NavLink
+            label="Grupos"
+            active={pathname === "/grupos"}
+            leftSection={<IconUsersGroup />}
+            fw="bolder"
+          />
+          <NavLink
+            label="Profesores"
+            active={pathname === "/profesores"}
+            leftSection={<IconUserStar />}
+            fw="bolder"
+          />
+          <NavLink
+            label="Servicios"
+            active={pathname === "/servicios"}
+            leftSection={<IconReceipt2 />}
+            fw="bolder"
+          />
         </AppShell.Navbar>
         <AppShell.Main></AppShell.Main>
       </AppShell>
