a1e7456b690297777ab1360bdc9cf403be8d9866
Francisco Payés <payesfrancisco@gmail.com>
2024-11-29 15:01:04 -0400

    initial app

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from "react-router";
 import './index.css'
 import App from './App.jsx'
+import '@mantine/core/styles.css';
+
+import { MantineProvider, createTheme } from '@mantine/core';
+
+const theme = createTheme({});
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <MantineProvider theme={theme}>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </MantineProvider>
+  </BrowserRouter>
 )
